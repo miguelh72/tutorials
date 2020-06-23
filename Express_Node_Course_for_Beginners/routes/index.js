255cd7af8b04fff0decf491e91a01a31b8074ece
Miguel Hernandez <miguelh72@outlook.com>
2020-06-23 14:00:15 -0400

    Added nodemon dev env.

--- Express_Node_Course_for_Beginners/routes/index.js
+++ Express_Node_Course_for_Beginners/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const index = require("../controller/index");
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index);
 
 module.exports = router;
