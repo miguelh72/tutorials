0247efa122451260ad6fb28e3ea5c343c10fbd83
Miguel Hernandez <miguelh72@outlook.com>
2020-06-30 22:36:20 -0400

    Pushing current state to save for system reinstall.

--- Express_Node_Course_for_Beginners/routes/index.js
+++ Express_Node_Course_for_Beginners/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const index = require("../controller/index");
+const landing = require("../controller/landing");
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', index);
+router.get('/', landing.get_landing);
+
+throw new Error("Working on video at 24:05");
 
 module.exports = router;
